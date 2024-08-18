5b8354481225ffc06a03a8a20370479ad40edfd5
Yaser Alhosani <yaser.alhosani@outlook.com>
2024-08-18 14:45:58 +0400

    Update wp-plugin-theme-report-generator.php

--- plugin-development/wp-plugin-theme-report-generator.php
+++ plugin-development/wp-plugin-theme-report-generator.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Name: WP Plugin & Theme Report Generator
+ *
  * Description: This snippet adds a tool to the WordPress admin area that allows site administrators to
  * export detailed reports of all installed plugins and themes, including their active/inactive status,
  * in both HTML and CSV formats. The exported files include summary metadata such as the total number of
